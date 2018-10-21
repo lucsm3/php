5706b441c57e3f70ecd330504bb1e2f797682be9
Lucas m <lucsm3@gmail.com>
2018-10-21 15:57:34 -0300

    resolvido o problema

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-echo "meu primeiro codigo php! <strong> Hello world </strong>" ;
-=======
+
 echo "meu primeiro codigo php! <strong> Ola Mundo  </strong>" ;
->>>>>>> b3d116aee33592194eaa015a822e031c1d96594d
+
 
 ?>
